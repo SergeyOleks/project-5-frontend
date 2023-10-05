6b1244ce68f0c783d30ae1ccd2bcfe803b0a670a
HannaTverdokhleb <84972134+HannaTverdokhleb@users.noreply.github.com>
2023-10-05 13:56:01 +0000

    Deploying to gh-pages from @ HannaTverdokhleb/project-5-frontend@21ff0601bdabdd51761b6fe5d5c059eb4ca89d1b 🚀

--- static/js/158.ebbe283a.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkproject_5_frontend=self.webpackChunkproject_5_frontend||[]).push([[158],{4158:function(e,n,c){c.r(n);var r=c(9439),t=c(7689),o=c(2791),u=c(9434),f=c(5122),s=c(184);n.default=function(){var e=(0,u.I0)();(0,o.useEffect)((function(){e((0,f.l)("User Profile"))}),[e]);var n=(0,t.bx)(),c=(0,r.Z)(n,1)[0];return console.log(c),(0,s.jsx)("div",{className:"accountPage",children:"Account"})}}}]);
-//# sourceMappingURL=158.ebbe283a.chunk.js.map
